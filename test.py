84930ad697d0c7a8c77781aa93fe325b0845c787
Dev Chaudhary <devc1829@gmail.com>
2024-05-22 19:36:29 +0530

    calculator

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)  # This (app) is the object
+
+
+@app.route('/')
+def welcome():
+    return "Welcome to Flask"
+
+
+
+
+print(__name__)
+if __name__ == "__main__":
+    app.run()
 
